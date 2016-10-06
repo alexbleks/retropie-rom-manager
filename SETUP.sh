a9640c7bfb3e9d6644cc57d294728ea77f8dd820
Alexander Blohme <alexander.blohme@gmail.com>
2016-10-06 21:25:24 +0000

    added n64 to setup file

--- SETUP.sh
+++ SETUP.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-echo "INSTALLING RETROBOX ROM MANAGER V0.1 BETA!"
+echo "INSTALLING RETROBOX ROM MANAGER V1.0 - RELEASE"
 echo "Apt-getting apache2,php5,php5-sqlite,php5gd,unzip"
 apt-get install apache2 php5 php5-sqlite php5-gd unzip
 echo "Creating folder structures..."
@@ -12,11 +12,13 @@ mkdir /home/pi/GAMEBOY
 mkdir /home/pi/GAMEBOY/roms
 mkdir /home/pi/GAMEBOY_COLOR
 mkdir /home/pi/GAMEBOY_COLOR/roms
+mkdir /home/pi/NINTENDO_64/roms
 echo "Moving images to each folder..."
-mv imgs/NES/* /home/pi/NES/roms/
-mv imgs/SNES/* /home/pi/SNES/roms/
-mv imgs/GB/* /home/pi/GAMEBOY/roms/
-mv imgs/GBC/* /home/pi/GAMEBOY_COLOR/roms/
+cp imgs/NES/* /home/pi/NES/roms/
+cp imgs/SNES/* /home/pi/SNES/roms/
+cp imgs/GB/* /home/pi/GAMEBOY/roms/
+cp imgs/GBC/* /home/pi/GAMEBOY_COLOR/roms/
+cp imgs/N64/* /home/pi/NINTENDO_64/roms/
 echo "Copying apache config files..."
 cp apache/000-default /etc/apache2/sites-enabled
 echo "Copying database file..."
@@ -25,6 +27,8 @@ echo "Copying web files..."
 cp www/* /home/pi/
 echo "Setting permissions..."
 chmod -R 777 /home/pi/
+echo "Overwriting your existing es_systems to work with this great manager..."
+cp es/es_systems.cfg /etc/emulationstation/
 
 echo "Done! Open http://your_raspberry_ip/ to manage your roms."
 echo "Also, you can copy roms over to /home/pi/<console_title>/roms folder via FTP, but remember to go to Rebuild Gamelist on web page after they have been copied over"
